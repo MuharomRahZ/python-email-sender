174f8fcbaa7b73e729d8193d6ffad25a6b11610a
MuharomRahZ <rahzaki05@gmail.com>
2022-05-11 21:21:11 +0800

    add modif main.py - main part email sender

--- main.py
+++ main.py
@@ -1,36 +1,38 @@
-import smtplib, ssl, getpass
+import smtplib, ssl, getpass, csv
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-gmail_sender = input(str("Masukkan akun gmail pengirim: "))
-password_sender = getpass.getpass(prompt="Masukkan password pengirim (tersembunyi): ")
+gmail_sender = input(str("Masukkan akun gmail pengirim: ")) #variabel pengirim
+password_sender = getpass.getpass(prompt="Masukkan password pengirim (tersembunyi): ") #variabel password pengirim
+sent_subject = input(str("Masukkan subjek email: ")) #variabel subject
+sent_body = input(str("Masukkan isi pesan email: ")) #variabel body
 
 
-context = ssl.create_default_context() #membuat protokol pengiriman email dengan default
-with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server: #mengatur profil protokol server
-    server.ehlo() #menetapkan localhost
-    server.login(gmail_sender, password_sender) #menetapkan email & pass user/sender
-    with open(r'C:\Users\ZAKI\Documents\AAA.Computing Projects\A.Zek-github-repositories-data\python-email-sender\receiver_list.txt', 'r') as file: #membaca file rceiver
-        gmail_receiver = file.read().replace('\n', ',') #mengatur variabel sebagai receiver
+#main-program / sending email part
+try:
+    file = open(r'C:\Users\ZAKI\Documents\AAA.Computing Projects\A.Zek-github-repositories-data\python-email-sender\receiver_list.txt', 'r') #membuka file receiver_list.txt
+    receiver = csv.reader(file) #variabel receiver email
+    for data in receiver: #untuk setiap data receiver dilakukan
+        message = MIMEMultipart('alternative') #penggunaan MIMEMultipart sebagai template pesan
+        message['Subject'] = sent_subject #menyertakan subjek email
+        message['From'] = gmail_sender #menyertakan pengirim email
+        message['To'] = data[1] #menyertakan penerima email
 
-        sent_from = gmail_sender #variabel sender
-        sent_to = gmail_receiver #variabel receiver
-        sent_subject = input(str("Masukkan subjek email: ")) #variabel subject
-        sent_body = input(str("Masukkan isi pesan email: ")) #variabel body
+        bodyPart = sent_body #variabel penampung bodypart email
 
-        #deklarasi body email
-        message = """\
-        from: %s
-        to: %s
-        subject: %s
+        message.attach(MIMEText(bodyPart, 'plain')) #menyertakan bodypart pesan email
 
-        %s
-        """ % (sent_from, ", ".join(sent_to), sent_subject, sent_body)
+        context = ssl.create_default_context() #membuat protokol pengiriman email dengan default
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server: #mengatur profil protokol server
+            try: #dilakukan
+                server.ehlo() #menetapkan localhost
+                server.login(gmail_sender, password_sender) #menetapkan akun user pengirim
+                server.sendmail(gmail_sender, data[1], msg=message.as_string()) #menetapkan pengirim, penerima, & isi pesan email
+                server.close() #setelah selesai mengirim, server akan berhenti
 
-        try:
-            server.sendmail(
-                sent_from, sent_to, message
-            )
-            server.close()
-
-            print('Email sent!')
-        except Exception as exception:
-            print("Error: %s!\n\n" % exception)
+                print('Email Kepada '+data[0]+' Berhasil Terkirim!') #menyampaikan pesan berhasil terkirim
+            except Exception as exception: #exception handling - jika try di atas error
+                print("Error: %s!\n\n" % exception) #menyertakan / memberitahukan alasan error try
+    file.close() #data selesai
+except Exception as exception: #exception handling - jika try di atas error
+    print("Error: %s!\n\n" % exception) #menyertakan / memberitahukan alasan error try
